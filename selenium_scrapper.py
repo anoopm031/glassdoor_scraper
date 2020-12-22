5a28a6497e6d26734464cb3e6ba74edb808aab69
anoopm031 <anoopm031@gmail.com>
2020-12-22 12:19:04 +0530

    final dataset and corresponding notebook in cleaning folder

--- selenium_scrapper.py
+++ selenium_scrapper.py
@@ -9,8 +9,8 @@ from soupify import write_to_csv   # created module
 
 
 CHROME_PATH = r'C:\Users\user\chromedriver.exe'
-username = 'username here'
-password = 'password here'
+username = 'physics0031@gmail.com'
+password = 'enthinovendi'
 url = 'https://www.glassdoor.co.in/Salaries/index.htm'
 jobs = ['Machine Learning Engineer', 'Data Scientist', 'Research Scientist',
         'Software Engineer', 'Business Analyst', 'Product Manager',
